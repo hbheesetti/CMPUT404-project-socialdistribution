11a736879b97ca7ccee9e41a0b5586a84c86b307
Peter <sqiu@ualberta.ca>
2023-03-30 22:55:27 -0600

    delete old stuff

--- backend/social/serveradmin/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-from django.http import HttpResponse
-from django.shortcuts import render, redirect
-# from .forms import *
- 
-# Create your views here.
--- backend/social/social/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from django.contrib.auth.models import User
-
-class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(min_length = 8, write_only = True)
-
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'password')
